5467f2fac7477dcb61d0d132bdf03e64353b1ace
Jørn <jorn.letnes@walwil.com>
2024-11-25 15:23:29 +0100

    streaming example that works against the producer

--- streaming/Program.cs
+++ streaming/Program.cs
@@ -1,25 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Streamiz.Kafka.Net;
+using Streamiz.Kafka.Net.Processors;
 using Streamiz.Kafka.Net.SerDes;
 using Streamiz.Kafka.Net.Stream;
 using Streamiz.Kafka.Net.Table;
 
 class Streams
 {
-    static async System.Threading.Tasks.Task Main(string[] args)
+    static async Task Main(string[] args)
     {
         var config = new StreamConfig<StringSerDes, StringSerDes>();
-        config.ApplicationId = "test-app";
+        config.ApplicationId = "test-app2";
         config.BootstrapServers = "localhost:9092";
 
         StreamBuilder builder = new StreamBuilder();
 
-        var kstream = builder.Stream<string, string>("stream");
-        var ktable = builder.Table("table", InMemory.As<string, string>("table-store"));
+        var kstream = builder.Stream<string, string>("purchases");
+        Action<string,string> PeekAction = (key, value) => 
+        {
+            Console.WriteLine($"Payload: {key} - {value}");
+        };
+        string ToAction(string key, string value, IRecordContext recordContext)
+        {
+            Console.WriteLine($"Payload: {value} - {recordContext}");
+            return value + "-" + recordContext;
+        }
 
-        kstream.Join(ktable, (v, v1) => $"{v}-{v1}")
-            .To("join-topic");
+        kstream.Peek(PeekAction);
+        kstream.To(ToAction);
 
         Topology t = builder.Build();
         KafkaStream stream = new KafkaStream(t, config);
